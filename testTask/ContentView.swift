822b45353fca4975c3b1bb3731c2bba2438553f8
altmurzilka <altmurzilka@gmail.com>
2020-05-21 03:59:25 +0600

    initial commit

---
+++ testTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testTask
+//
+//  Created by алтынпончик on 5/21/20.
+//  Copyright © 2020 алтынпончик. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
